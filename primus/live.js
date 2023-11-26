d9c92cdacd472a545fa445b302de487b3863beec
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-11-26 21:50:39 +0100

    feat: add server setup with primus

---
+++ primus/live.js
@@ -0,0 +1,12 @@
+module.exports.go = (server) => {
+    const Primus = require("primus");
+    const primus = new Primus(server);
+  
+    //check if connection, then console.log it
+    primus.on("connection", (spark) => {
+        console.log("connection alive");
+        spark.on("data", (data) => {
+            console.log(data);
+        });
+    });
+};
